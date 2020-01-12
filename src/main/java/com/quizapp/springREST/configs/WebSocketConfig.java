fb533b4cb2b78d8362136e3e126c7d8db80eb027
mandrysiak <andrysiakmaciejj@gmail.com>
2020-01-12 22:20:04 +0100

    log fix

--- src/main/java/com/quizapp/springREST/configs/WebSocketConfig.java
+++ src/main/java/com/quizapp/springREST/configs/WebSocketConfig.java
@@ -1,27 +1,20 @@
 package com.quizapp.springREST.configs;
 
+import com.quizapp.springREST.controllers.WebSocketHandler;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
 @EnableWebSocketMessageBroker
 @EnableScheduling
 // Add this annotation to an @Configuration class to configure processing WebSocket requests
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-
-
-        registry.addEndpoint("/live");
-    }
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/games");
-        registry.setApplicationDestinationPrefixes("/app");
-    }
+    public class WebSocketConfig implements WebSocketConfigurer {
+        @Override
+        public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+            registry.addHandler(new WebSocketHandler(), "/websocket");
+        }
 }
+
---
+++ src/main/java/com/quizapp/springREST/controllers/WebSocketHandler.java
@@ -0,0 +1,16 @@
+package com.quizapp.springREST.controllers;
+
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.AbstractWebSocketHandler;
+import java.io.IOException;
+public class WebSocketHandler extends AbstractWebSocketHandler {
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
+        String msg = String.valueOf(message.getPayload());
+        // Send back unique message depending on the id received from the client
+
+        System.out.println( String.valueOf(message.getPayload()) + "=========================================================================================");
+        session.sendMessage(new TextMessage("XD"));
+        }
+    }
