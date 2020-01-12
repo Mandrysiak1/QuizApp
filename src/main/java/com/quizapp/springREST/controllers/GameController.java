ffe2595b1f58cdc5e15cbba5fcdee4811da7eb25
mandrysiak <andrysiakmaciejj@gmail.com>
2020-01-12 23:04:18 +0100

    log fix

--- src/main/java/com/quizapp/springREST/controllers/GameController.java
+++ src/main/java/com/quizapp/springREST/controllers/GameController.java
@@ -1,47 +1,47 @@
-//package com.quizapp.springREST.controllers;
-//
-//import org.springframework.stereotype.Controller;
-//
-//@Controller
-//public class GameController {
-//
-//
-////    @Autowired
-////    GameService service;
-////
-////
-////    @SubscribeMapping("/games/{game_id}")
-////    public String subscribeToGame(
-////            @DestinationVariable(value = "game_id") String game_id) {
-////
-////        return "aaa " + game_id;
-////    }
-////
-////
-////    @MessageMapping("/games/{game_id}/start")
-////    @SendTo("/games/{game_id}")
-////    public GameState startGame(
-////            @DestinationVariable(value = "game_id") String game_id){
-////
-////             return service.startNewGame(game_id).getGameState();
-////    }
-////
-////    @MessageMapping("/games/{game_id}/ans")
-////    public void sendAnserws(@DestinationVariable(value = "game_id") String game_id,
-////                            @Payload PlayerAnswers answers){
-////
-////            service.proceedAnserws(game_id,answers);
-////
-////    }
-////
-////    public void nextRound(String game_id){
-////
-////        service.sendMessage("/games/{game_id}",service.GetGameByGameID(game_id).startNextRound());
-////      //this.template.convertAndSend("/games/{game_id}",service.GetGameByGameID(game_id).startNextRound());
-////    }
-////
+package com.quizapp.springREST.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GameController {
+
+
+//    @Autowired
+//    GameService service;
 //
 //
-//}
+//    @SubscribeMapping("/games/{game_id}")
+//    public String subscribeToGame(
+//            @DestinationVariable(value = "game_id") String game_id) {
+//
+//        return "aaa " + game_id;
+//    }
 //
 //
+//    @MessageMapping("/games/{game_id}/start")
+//    @SendTo("/games/{game_id}")
+//    public GameState startGame(
+//            @DestinationVariable(value = "game_id") String game_id){
+//
+//             return service.startNewGame(game_id).getGameState();
+//    }
+//
+//    @MessageMapping("/games/{game_id}/ans")
+//    public void sendAnserws(@DestinationVariable(value = "game_id") String game_id,
+//                            @Payload PlayerAnswers answers){
+//
+//            service.proceedAnserws(game_id,answers);
+//
+//    }
+//
+//    public void nextRound(String game_id){
+//
+//        service.sendMessage("/games/{game_id}",service.GetGameByGameID(game_id).startNextRound());
+//      //this.template.convertAndSend("/games/{game_id}",service.GetGameByGameID(game_id).startNextRound());
+//    }
+//
+
+
+}
+
+
--- src/main/java/com/quizapp/springREST/controllers/WebSocketHandler.java
+++ src/main/java/com/quizapp/springREST/controllers/WebSocketHandler.java
@@ -1,11 +1,21 @@
 package com.quizapp.springREST.controllers;
 
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.AbstractWebSocketHandler;
+
 import java.io.IOException;
 
 public class WebSocketHandler extends AbstractWebSocketHandler {
+
+    @SubscribeMapping("/games/{game_id}")
+    public String subscribeToGame(
+            @DestinationVariable(value = "game_id") String game_id) {
+
+        return "aaa " + game_id;
+    }
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
         String msg = String.valueOf(message.getPayload());
