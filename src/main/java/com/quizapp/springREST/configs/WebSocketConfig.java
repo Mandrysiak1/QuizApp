3be625a2b46f57aa3c6a1f941fb3a0037cddc58e
mandrysiak <andrysiakmaciejj@gmail.com>
2020-01-16 19:09:23 +0100

    work

--- src/main/java/com/quizapp/springREST/configs/WebSocketConfig.java
+++ src/main/java/com/quizapp/springREST/configs/WebSocketConfig.java
@@ -1,24 +1,19 @@
 package com.quizapp.springREST.configs;
 
+import com.quizapp.springREST.controllers.WebSocketHandler;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
-
+@EnableWebSocket
+// Add this annotation to an @Configuration class to configure processing WebSocket requests
+public class WebSocketConfig implements WebSocketConfigurer {
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
-    }
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/hello").withSockJS();
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new WebSocketHandler(), "/websocket");
     }
 }
+
 
--- src/main/java/com/quizapp/springREST/controllers/WebSocketHandler.java
+++ src/main/java/com/quizapp/springREST/controllers/WebSocketHandler.java
@@ -1,27 +1,21 @@
 package com.quizapp.springREST.controllers;
 
-import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.simp.annotation.SubscribeMapping;
+
+import org.springframework.web.socket.BinaryMessage;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.AbstractWebSocketHandler;
-
 import java.io.IOException;
-
 public class WebSocketHandler extends AbstractWebSocketHandler {
+    @Override
+    protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) throws Exception {
+        super.handleBinaryMessage(session, message);
 
-    @SubscribeMapping("/games/123")
-    public String subscribeToGame(
-            @DestinationVariable(value = "game_id") String game_id) {
-
-        return "aaa " + 123;
     }
+
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
         String msg = String.valueOf(message.getPayload());
-        // Send back unique message depending on the id received from the client
-
-        System.out.println( String.valueOf(message.getPayload()) + "=========================================================================================");
-        session.sendMessage(new TextMessage("XD"));
-        }
+        session.sendMessage(new TextMessage(msg + "XD"));
     }
+}
