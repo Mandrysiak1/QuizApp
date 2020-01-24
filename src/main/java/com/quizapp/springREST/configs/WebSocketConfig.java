18f5f84da04ebf0f28ee3e5b2e7909fd52f1acb9
mandrysiak <andrysiakmaciejj@gmail.com>
2020-01-24 20:31:48 +0100

    ws part 123

--- src/main/java/com/quizapp/springREST/configs/WebSocketConfig.java
+++ src/main/java/com/quizapp/springREST/configs/WebSocketConfig.java
@@ -12,27 +12,25 @@ package com.quizapp.springREST.configs;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocket
-@EnableWebSocketMessageBroker
-class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic", "/queue", "/exchange");
-//        config.enableStompBrokerRelay("/topic", "/queue", "/exchange"); // Uncomment for external message broker (ActiveMQ, RabbitMQ)
-        config.setApplicationDestinationPrefixes("/topic", "/queue"); // prefix in client queries
-        config.setUserDestinationPrefix("/user");
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/live");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/example-endpoint").withSockJS();
+        registry.addEndpoint("/websocket");
     }
 }
 
