96bece54267f6a74a2fac3e2c38c838e79a2dba6
mandrysiak <andrysiakmaciejj@gmail.com>
2020-01-27 17:43:32 +0100

    aaa

--- src/main/java/com/quizapp/springREST/controllers/SocietiesController.java
+++ src/main/java/com/quizapp/springREST/controllers/SocietiesController.java
@@ -34,7 +34,7 @@ public class SocietiesController {
         societyManager.saveSociety(newSociety);
     }
 
-    @PostMapping("/getUserSocieties")
+        @PostMapping("/getUserSocieties")
     public ResponseEntity getUserSocieties(@RequestBody SocietyRequest login)
     {
         SocietyResponse societies;
