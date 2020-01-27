3b18b869a7a48d2ce2dff7a14cbd3c0bdd145458
mandrysiak <andrysiakmaciejj@gmail.com>
2020-01-27 17:50:13 +0100

    aaa

--- src/main/java/com/quizapp/springREST/controllers/SocietyRequest.java
+++ src/main/java/com/quizapp/springREST/controllers/SocietyRequest.java
@@ -1,7 +1,16 @@
 package com.quizapp.springREST.controllers;
 
 public class SocietyRequest {
-    String name;
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public SocietyRequest(String name) {
         this.name = name;
