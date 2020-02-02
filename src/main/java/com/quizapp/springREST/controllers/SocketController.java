61a765dd3eb6dcb949ec9defc2747616c2c0a090
mandrysiak <andrysiakmaciejj@gmail.com>
2020-02-02 02:31:15 +0100

    :as

--- src/main/java/com/quizapp/springREST/controllers/SocketController.java
+++ src/main/java/com/quizapp/springREST/controllers/SocketController.java
@@ -1,15 +1,18 @@
 package com.quizapp.springREST.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.messaging.simp.user.SimpUserRegistry;
+import org.springframework.messaging.support.GenericMessage;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.socket.messaging.SessionSubscribeEvent;
 
 @RestController
 class SocketController {
@@ -33,5 +36,16 @@ class SocketController {
     @RequestMapping(value = "/hello-convert-and-send", method = RequestMethod.POST)
     void echoConvertAndSend(@RequestParam("msg") String message) {
         //socketService.echoMessage(message);
+    }
+
+    @EventListener
+    public void handleSessionSubscribeEvent(SessionSubscribeEvent event) {
+        GenericMessage message = (GenericMessage) event.getMessage();
+        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA + SUBSKRYPCJA " + message);
+        String simpDestination = (String) message.getHeaders().get("simpDestination");
+
+        if (simpDestination.startsWith("/topic/group/1")) {
+            // do stuff
+        }
     }
 }
