b3c7e528da76628235955143a0205ed8dd44b380
mandrysiak <andrysiakmaciejj@gmail.com>
2020-02-02 02:39:05 +0100

    as

--- src/main/java/com/quizapp/springREST/controllers/SocketController.java
+++ src/main/java/com/quizapp/springREST/controllers/SocketController.java
@@ -3,6 +3,7 @@ package com.quizapp.springREST.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.messaging.simp.user.SimpUserRegistry;
@@ -21,7 +22,7 @@ class SocketController {
 
     @Autowired private SimpUserRegistry simpUserRegistry;
 
-    @SubscribeMapping("/games/game_id")
+    @MessageMapping("/games/{game_id}")
     @SendTo("/topic/games/{game_id}")
     EchoModel echoMessageMapping(@DestinationVariable String game_id, String message) {
        String x = message + game_id + "ASADADADADSADSAD===============================================================================================";
