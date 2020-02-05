4621b23435ba6459f26597cec0006f5dd08521aa
mandrysiak <andrysiakmaciejj@gmail.com>
2020-02-05 17:07:00 +0100

    ass

--- src/main/java/com/quizapp/springREST/services/SocketService.java
+++ src/main/java/com/quizapp/springREST/services/SocketService.java
@@ -1,5 +1,6 @@
 package com.quizapp.springREST.services;
 
+import com.quizapp.springREST.model.EchoModel;
 import com.quizapp.springREST.model.objects.GameState;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -17,8 +18,11 @@ public class SocketService {
         {
             System.out.println("nie chce zyc" +
                     "");
+        }else if(game_id == null){
+            System.out.println("XDDXDXDXDXDXDXDXDXDXDXDXD");
         }
-        simpTemplate.convertAndSend("/topic/games/"+game_id , new GameState(message.getRoundNumber(),message.getQuestion(),message.getRanking()));
+        ///simpTemplate.convertAndSend("/topic/games/"+game_id , new GameState(message.getRoundNumber(),message.getQuestion(),message.getRanking()));
+        simpTemplate.convertAndSend("/topic/games/"+game_id , new EchoModel(game_id.trim());
 
     }
 }
